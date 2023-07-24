52ce8d95b5ac0bf479b4c75ef2724bd79cd4ee60
Jose De La Luz <Jdelaluz01@gmail.com>
2023-07-24 16:56:03 -0500

    movies and descriptons are displayed when searched

--- Javascript/Movie-Project-JS.js
+++ Javascript/Movie-Project-JS.js
@@ -33,7 +33,23 @@ searchContainer.addEventListener('keyup', () =>{
 searchBTN.addEventListener('click', async () =>{
     const events = await getUserInputMovie(userSearch);
     console.log(events);
-   const moviesFunction = (events) =>{
+    movieCards.innerHTML = ''
+    events.forEach((movie) =>{
+        let h2 = document.createElement('H2')
+        let p = document.createElement('p')
+        let addbtn = document.createElement('button')
+        let rmvbtn = document.createElement('button')
+        addbtn.classList.add('add-movie')
+        rmvbtn.classList.add('remove-movie')
+        addbtn.textContent = 'add this movie?'
+        rmvbtn.textContent = 'remove this movie?'
+        h2.textContent = movie.original_title
+        p.textContent = movie.overview
+        h2.classList.add('card')
+        h2.appendChild(p)
+        p.appendChild(addbtn)
+        p.appendChild(rmvbtn)
+        movieCards.appendChild(h2)
+    })
 
-   }
 })
