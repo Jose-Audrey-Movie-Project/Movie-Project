7e8871aa2621140daabad35b9cba46eb946b9208
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-07-25 14:49:21 -0500

    adding db json to new html fave movies

---
+++ Javascript/favorite-movies-page.js
@@ -0,0 +1,25 @@
+"use strict"
+const getMovieList = async (favMovies) => {
+    try{
+        const URL = `/Data/db.json`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(favMovies)
+        };
+        const response = await fetch(URL, options);
+        console.log(response);
+        const data = response.json();
+        console.log(data);
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+(async () => {
+    let getTheMovies = await getMovieList();
+
+})();
