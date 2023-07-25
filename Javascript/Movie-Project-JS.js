b028efa259a0989b494c99f381c64140089d820a
Jose De La Luz <Jdelaluz01@gmail.com>
2023-07-25 15:21:45 -0500

    workig with js functions

--- Javascript/Movie-Project-JS.js
+++ Javascript/Movie-Project-JS.js
@@ -28,7 +28,7 @@ const creatingMovieCards = async () => {
       <p>${movie.overview}</p>
       <div class="btn-holder d-flex justify-center align-center gap-2">
       <div class="btn-holder d-flex justify-center align-center gap-5">
-      <button class="add-movie btn" data-movieid="${movie.id}" data-movietitle="${movie.original_title}" data-popularity="${movie.popularity}">
+      <button class="add-movie btn" data-movieid="${movie.id}" data-movietitle="${movie.original_title}" data-popularity="${movie.popularity}" data-description="${movie.overview}">
        Add Movie!!
       </button>
        <button class="remove-movie btn">
@@ -44,16 +44,13 @@ const creatingMovieCards = async () => {
             button.addEventListener('click' , () => {
                 let ids = button.getAttribute('data-movieid')
                 let titles = button.getAttribute('data-movietitle')
+                let descriptions =  button.getAttribute('data-description')
                 let movie = {
                     id: ids,
-                    title: titles
+                    title: titles,
+                    description: descriptions
                 }
-                async function addingmov  (movie){
-                let adder = await postFavMovies(movie)
-                    return adder
-                }
-                addingmov();
-
+                postFavMovies(movie)
             })
         })
     })
@@ -77,8 +74,3 @@ searchContainer.addEventListener('keydown', (event) => {
 });
 
 
-movieCards.addEventListener('click', (event) => {
-    if (event.target.classList.contains('remove-movie')) {
-        console.log('remove movie clicked');
-    }
-});
