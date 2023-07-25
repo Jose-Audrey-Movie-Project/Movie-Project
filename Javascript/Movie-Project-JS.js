3b68b3e494b740b32a4119bfde5064250dc4c743
Jose De La Luz <Jdelaluz01@gmail.com>
2023-07-25 10:29:13 -0500

    local api to store user fav and post fav movie functionality

--- Javascript/Movie-Project-JS.js
+++ Javascript/Movie-Project-JS.js
@@ -4,9 +4,13 @@ global variables
  */
 ////////////////////////////////////////////////////////////////////////////////////////////
 let searchBTN = document.querySelector('#searchBTN');
-let userSearch =''
-let searchContainer = document.querySelector('#searchbar')
-let movieCards = document.querySelector('#movieCards')
+let userSearch ='';
+let searchContainer = document.querySelector('#searchbar');
+let movieCards = document.querySelector('#movieCards');
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 /*
 functions
@@ -33,23 +37,22 @@ searchContainer.addEventListener('keyup', () =>{
 searchBTN.addEventListener('click', async () =>{
     const events = await getUserInputMovie(userSearch);
     console.log(events);
-    movieCards.innerHTML = ''
+    movieCards.innerHTML = '';
     events.forEach((movie) =>{
-        let h2 = document.createElement('H2')
-        let p = document.createElement('p')
-        let addbtn = document.createElement('button')
-        let rmvbtn = document.createElement('button')
-        addbtn.classList.add('add-movie')
-        rmvbtn.classList.add('remove-movie')
-        addbtn.textContent = 'add this movie?'
-        rmvbtn.textContent = 'remove this movie?'
-        h2.textContent = movie.original_title
-        p.textContent = movie.overview
-        h2.classList.add('card')
-        h2.appendChild(p)
-        p.appendChild(addbtn)
-        p.appendChild(rmvbtn)
-        movieCards.appendChild(h2)
-    })
-
+        let h2 = document.createElement('H2');
+        let p = document.createElement('p');
+        let addbtn = document.createElement('button');
+        let rmvbtn = document.createElement('button');
+        addbtn.classList.add('add-movie');
+        rmvbtn.classList.add('remove-movie');
+        addbtn.textContent = 'add this movie?';
+        rmvbtn.textContent = 'remove this movie?';
+        h2.textContent = movie.original_title;
+        p.textContent = movie.overview;
+        h2.classList.add('card');
+        h2.appendChild(p);
+        p.appendChild(addbtn);
+        p.appendChild(rmvbtn);
+        movieCards.appendChild(h2);
+    });
 })
