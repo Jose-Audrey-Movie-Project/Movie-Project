09a57a59727f617fd433c48d2a2fc313f7143ff2
Jose De La Luz <Jdelaluz01@gmail.com>
2023-07-26 11:42:33 -0500

    trying the stars

--- Javascript/Movie-Project-JS.js
+++ Javascript/Movie-Project-JS.js
@@ -8,7 +8,6 @@ let userSearch = '';
 let searchContainer = document.querySelector('#searchbar');
 let movieCards = document.querySelector('#movieCards');
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////
 /*
 functions
@@ -20,11 +19,14 @@ const creatingMovieCards = async () => {
     const imageURL = `https://image.tmdb.org/t/p/original`;
     movieCards.innerHTML = '';
 
+
     events.forEach((movie) => {
         movieCards.innerHTML += `
 
 
-<div class="card">
+
+
+<div class="card m-2">
   <div class="card-title">${movie.original_title}</div>
     <div class="card-info">
     <div class="image-overview">
@@ -32,30 +34,37 @@ const creatingMovieCards = async () => {
       <p>${movie.overview}</p>
      </div>
       <div class="btn-holder d-flex justify-center align-center gap-2">
-      <button class="add-movie btn" data-img="${imageURL + movie.poster_path}" data-movieid="${movie.id}" data-movietitle="${movie.original_title}" data-popularity="${movie.popularity}" data-description="${movie.overview}">
+      <button class="add-movie btn m-2" data-img="${imageURL + movie.poster_path}" data-movieid="${movie.id}" data-movietitle="${movie.original_title}" data-popularity="${movie.popularity}" data-description="${movie.overview}" data-image="${imageURL + movie.poster_path}">
        Add Movie!!
       </button>
       </div>
 </div>
 
         `
+
         let addbtn = document.querySelectorAll('.add-movie')
-        addbtn.forEach( (button) => {
-            button.addEventListener('click' , () => {
+        addbtn.forEach((button) => {
+            button.addEventListener('click', () => {
                 let ids = button.getAttribute('data-movieid')
                 let titles = button.getAttribute('data-movietitle')
-                let descriptions =  button.getAttribute('data-description')
+                let descriptions = button.getAttribute('data-description')
+                let images = button.getAttribute('data-image')
                 let movie = {
                     id: ids,
+                    image: images,
                     title: titles,
                     description: descriptions,
+                    stars: `<div class="d-flex justify-content-center align-items-center m-2"><span data-star-id="1" class="fa fa-star"></span>
+<span data-star-id="2" class="fa fa-star"></span>
+<span data-star-id="3"  class="fa fa-star"></span>
+<span data-star-id="4"  class="fa fa-star"></span>
+<span data-star-id="5"  class="fa fa-star"></span></div>`
                 }
                 postFavMovies(movie);
             })
         })
     })
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 /*
@@ -74,3 +83,34 @@ searchContainer.addEventListener('keydown', (event) => {
 });
 
 
+/*
+stuff that didn't work
+const displaystars = (num) => {
+    if(num === 0 ){
+        let nostars =  `<div className=" d-flex justify-center align-center m-2"><span className="fa fa-star"></span><span
+        className="fa fa-star"></span><span className="fa fa-star"></span><span className="fa fa-star"></span><span
+        className="fa fa-star"></span></div>`
+        return movie.stars = nostars
+    } else if (num === 1) {
+        return  `<div className=" d-flex justify-center align-center m-2"><span className="fa fa-star checked"></span><span
+        className="fa fa-star"></span><span className="fa fa-star"></span><span className="fa fa-star"></span><span
+        className="fa fa-star"></span></div>`
+    } else if (num === 2) {
+        return  `<div className=" d-flex justify-center align-center m-2"><span className="fa fa-star checked"></span><span
+        className="fa fa-star checked"></span><span className="fa fa-star"></span><span className="fa fa-star"></span><span
+        className="fa fa-star"></span></div>`
+    } else if (num === 3) {
+        return  `<div className=" d-flex justify-center align-center m-2"><span className="fa fa-star checked"></span><span
+        className="fa fa-star checked"></span><span className="fa fa-star checked"></span><span className="fa fa-star"></span><span
+        className="fa fa-star"></span></div>`
+    } else if (num === 4) {
+        return  `<div className=" d-flex justify-center align-center m-2"><span className="fa fa-star checked"></span><span
+        className="fa fa-star checked"></span><span className="fa fa-star checked"></span><span className="fa fa-star checked"></span><span
+        className="fa fa-star"></span></div>`
+    } else if (num === 6) {
+        return  `<div className=" d-flex justify-center align-center m-2"><span className="fa fa-star checked"></span><span
+        className="fa fa-star checked"></span><span className="fa fa-star checked"></span><span className="fa fa-star checked"></span><span
+        className="fa fa-star checked"></span></div>`
+    }
+}
+ */
